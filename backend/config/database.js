e30e3b4bc8e3f26049878bde47561781b8881745
Fabio Cruz <fabinhocosta1202@gmail.com>
2018-02-13 22:14:10 -0200

    realizando testes de requsições

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,2 +1,4 @@
 const mongoose = require('mongoose')
 module.exports = mongoose.createConnection('mongodb://localhost/db_finance')
+
+mongoose.Error.messages.general.required = "The atribute '{PATH}' is required"
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,3 +1,4 @@
+
 const express = require('express')
 
 module.exports = function(server){
@@ -5,6 +6,12 @@ module.exports = function(server){
     // API ROUTES
     const router = express.Router()
     server.use('/api', router)
-    
 
-}
+    router.route('/teste').get(function(req, res, next){
+        res.send('funcionou')
+    })
+
+    // ROTAS DA API
+    const billingCycleService = require('../api/billingCycle/billingCycleService')
+    billingCycleService.register(router, '/billingCycles')
+}   
